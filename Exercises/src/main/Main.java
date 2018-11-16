3a871b9abf04fc1f109dcb86f25bb4fe2b1a9269
Illnoid <stefan2846@hotmail.com>
2018-11-16 12:47:45 +0100

    Lab1 intialize

--- Exercises/src/main/Main.java
+++ Exercises/src/main/Main.java
@@ -1,18 +1,15 @@
 package main;
 
-import java.util.Arrays;
+import main.lab1.MaxSum;
 
 public class Main {
 
     public static void main(String[] args) {
-        Exercise1 exercise1 = new Exercise1();
-        int[] array = new int[] {1,2,3,4,5,6,7,8,9};
-        int[] newArray = exercise1.pushIntToArray(array,11,4);
-        System.out.println(Arrays.toString(newArray));
 
-        String str1 = "stefan";
-        String str2 = "chan";
-        System.out.println(exercise1.weaveStringsTogether(str1, str2, new StringBuilder()));
+        MaxSum sum = new MaxSum();
+        int intSum = sum.maxSubSum1(new int[] {1,2,3,4,5});
+        System.out.println(intSum);
+
     }
 
 }
