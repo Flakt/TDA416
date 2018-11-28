834f5e5009f0a0e7e73be6142489a87429539a1e
Praxzy <pontus.lindblom@hotmail.com>
2018-11-28 14:11:55 +0100

    Begining of Lab2

--- src/Lab/Lab2/SLCWithGet.java
+++ src/Lab/Lab2/SLCWithGet.java
@@ -1,5 +1,27 @@
 package Lab.Lab2;
 
-public class SLCWithGet {
+public class SLCWithGet extends LinkedCollection {
+
+    SLCWithGet(){
+        super();
+    }
+
+    @Override
+    public boolean add(Object element) {
+        if (head == null) {
+            head = new Entry(element, null);
+            return true;
+        } else {
+            addObject(head, element);
+        }
+    }
+
+    private boolean addObject(Entry entry, Object element) {
+        if ()
+            element.
+    }
+
+
+
 
 }
