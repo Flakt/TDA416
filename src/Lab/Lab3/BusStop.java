685289b6a09119fa1b4bbd00cd8381f597f84fdf
Praxzy <pontus.lindblom@hotmail.com>
2018-12-10 09:31:10 +0100

    Added source files and added package declarations to them

---
+++ src/Lab/Lab3/BusStop.java
@@ -0,0 +1,11 @@
+package Lab.Lab3;
+
+public class BusStop extends NodeObject {
+  int xpos, ypos;
+  
+  BusStop(String name, int xpos, int ypos) {
+    super(name);
+    this.xpos = xpos;
+    this.ypos = ypos;
+  }
+}
