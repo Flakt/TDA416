4c9626583e28f815691b48d0ad693e6f0426fcbb
illnoid <stefan2846@hotmail.com>
2018-12-14 13:22:37 +0100

    Preliminary version of compKruskalCompare

--- src/Lab/Lab3/CompKruskalEdge.java
+++ src/Lab/Lab3/CompKruskalEdge.java
@@ -1,6 +1,52 @@
 package Lab.Lab3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class CompKruskalEdge<E extends Edge> {
+    private PriorityQueue<E> prioQ = new PriorityQueue<>();
+    private List<List<E>> connections = new ArrayList<>();
+
+    CompKruskalEdge() {
+
+    }
+
+    private void insertEdgesIntoQueue(List<E> edges) {
+        for (E edge : edges) {
+            prioQ.offer(edge);
+        }
+    }
 
 
+    private void insertEdgeIntoCc(E edge, int index) {
+        connections.get(index).add(edge);
+    }
+
+    public List compare(List<E> edges) {
+        insertEdgesIntoQueue(edges);
+
+        while (!(prioQ.isEmpty()) && connections.size() > 1) {
+            Edge edge = prioQ.poll();
+
+            if (connections.get(edge.from - 1).size() < connections.get(edge.to - 1).size()) {
+                moveEdges(edge.to, connections.get(edge.from - 1));
+                insertEdgeIntoCc((E) edge, edge.to);
+            }
+            else {
+                moveEdges(edge.from, connections.get(edge.to - 1));
+                insertEdgeIntoCc((E) edge, edge.from);
+            }
+        }
+
+        return connections;
+    }
+
+
+    private void moveEdges(int index, List<E> edges) {
+        for (E edge : edges) {
+            connections.get(index - 1).add(edge);
+        }
+    }
+    
 }
